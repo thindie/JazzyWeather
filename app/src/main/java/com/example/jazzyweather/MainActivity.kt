040c096ea44347df7d23e8a54bd8773261a5f045
Alex <thindie1@mail.ru>
2023-02-26 21:05:28 +0200

    Set ComposeTheme by M3 theme generator. Set Font

---
+++ app/src/main/java/com/example/jazzyweather/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.jazzyweather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
+import com.example.jazzyweather.ui.theme.JazzyWeatherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        setContent {
+            JazzyWeatherTheme {
+
+
+            }
+        }
+    }
+}
