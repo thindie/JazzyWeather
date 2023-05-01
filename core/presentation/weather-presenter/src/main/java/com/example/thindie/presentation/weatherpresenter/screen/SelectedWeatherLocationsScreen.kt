a7ef8564c29c7d5ede3ecd5b44d986632d961457
Alex <thindie1@mail.ru>
2023-05-01 11:50:39 +0200

    seed presentation screens

---
+++ core/presentation/weather-presenter/src/main/java/com/example/thindie/presentation/weatherpresenter/screen/SelectedWeatherLocationsScreen.kt
@@ -0,0 +1,8 @@
+package com.example.thindie.presentation.weatherpresenter.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SelectedWeatherLocationsScreen(isWideScreen: Boolean){
+
+}
---
+++ core/presentation/weather-presenter/src/main/java/com/example/thindie/presentation/weatherpresenter/screen/WeatherPresenterScreen.kt
@@ -0,0 +1,8 @@
+package com.example.thindie.presentation.weatherpresenter.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WeatherPresenterScreen(isWideScreen: Boolean){
+
+}
