33e4f539326a39bdc3fdce626b018654c871854e
Alex <thindie1@mail.ru>
2023-09-06 00:03:51 +0200

    refactor design-system module
    add drawables, delete useless compose funct-s, add theme, add font

--- core/presentation/design-system/src/main/java/com/example/thindie/presentation/designsystem/BarFiller.kt
+++
@@ -1,8 +0,0 @@
-package com.example.thindie.presentation.designsystem
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-interface BarFiller {
-    val icon: ImageVector
-    val route: String
-}
--- core/presentation/design-system/src/main/java/com/example/thindie/presentation/designsystem/ButtonFiller.kt
+++
@@ -1,6 +0,0 @@
-package com.example.thindie.presentation.designsystem
-
-interface ButtonFiller {
-    val label: String
-    val onClick: (String) -> Unit
-}
