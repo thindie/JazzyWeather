43749dbe0402a5f8322bcde3b3033d2d56793600
Alex <thindie1@mail.ru>
2023-09-07 20:07:34 +0200

    Add design-system module

---
+++ core/design-system/src/main/java/com/example/thindie/designsystem/utils/weatherCodeToDrawable.kt
@@ -0,0 +1,34 @@
+package com.example.thindie.designsystem.utils
+
+import com.example.thindie.presentation.R
+
+internal val weatherCodeToDrawableRes = mapOf(
+    0 to R.drawable.weather_sunny,
+    1 to R.drawable.weather_partly_cloudy,
+    2 to R.drawable.weather_partly_cloudy,
+    3 to R.drawable.weather_partly_rainy,
+    45 to R.drawable.weather_fog,
+    48 to R.drawable.weather_fog,
+    51 to R.drawable.weather_hail,
+    53 to R.drawable.weather_hail,
+    55 to R.drawable.weather_hail,
+    56 to R.drawable.weather_partly_snowy,
+    57 to R.drawable.weather_partly_snowy,
+    61 to R.drawable.weather_partly_rainy,
+    63 to R.drawable.weather_rainy,
+    65 to R.drawable.weather_rainy,
+    66 to R.drawable.snowflake_melt,
+    67 to R.drawable.snowflake_melt,
+    71 to R.drawable.weather_partly_snowy,
+    73 to R.drawable.weather_snowy,
+    75 to R.drawable.weather_snowy_heavy,
+    77 to R.drawable.snowflake_alert,
+    80 to R.drawable.weather_pouring,
+    81 to R.drawable.weather_pouring,
+    82 to R.drawable.weather_pouring,
+    85 to R.drawable.snowflake_alert,
+    86 to R.drawable.snowflake_alert,
+    95 to R.drawable.weather_partly_lightning,
+    96 to R.drawable.weather_lightning,
+    99 to R.drawable.flash_alert_outline,
+)
