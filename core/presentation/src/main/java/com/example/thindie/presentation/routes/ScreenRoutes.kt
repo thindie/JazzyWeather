d15e5815b945cccb4b2abefa57321d016e9edc1d
Alex <thindie1@mail.ru>
2023-09-07 21:01:18 +0200

    add presentation module-package, add location-presenter module

--- core/presentation/src/main/java/com/example/thindie/presentation/routes/ScreenRoutes.kt
+++
@@ -1,7 +0,0 @@
-package com.example.thindie.presentation.routes
-
-object WeatherRoutes {
-    const val weather = "weather"
-    const val possiblyLocation = "possibly"
-    const val weatherConcreteLocation = "concrete"
-}
