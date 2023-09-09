3fa9ed90726d6b6409ba24b198e92abf2f24b235
Alex <thindie1@mail.ru>
2023-09-09 15:52:56 +0200

    add naive location picker model, location picker screen

--- core/presentation/location-presenter/src/main/java/com/example/thindie/location_presenter/screen/LocationPickerScreen.kt
+++ core/presentation/location-presenter/src/main/java/com/example/thindie/location_presenter/screen/LocationPickerScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
@@ -17,9 +16,9 @@ import com.example.thindie.designsystem.composables.inputfield.InputFieldState
 import com.example.thindie.designsystem.composables.inputfield.rememberInputFieldState
 import com.example.thindie.designsystem.theme.JazzyWeatherTheme
 import com.example.thindie.location_presenter.components.WeatherInputField
+import com.example.thindie.location_presenter.components.WeatherLocationPickerUnit
 import com.example.thindie.location_presenter.viewmodel.LocationPickerViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun LocationPickerScreen(
     modifier: Modifier = Modifier,
@@ -31,8 +30,11 @@ internal fun LocationPickerScreen(
     viewModel.onSearchReact(inputFieldState.fieldValue.value.value)
     Scaffold(topBar = { WeatherInputField(state = inputFieldState) }) {
         LazyColumn(modifier = modifier.padding(it)) {
-            items(state.value.locationsList) {
-                Text(text = it.adminName)
+            items(state.value.locationsList) { location ->
+                WeatherLocationPickerUnit(
+                    location = location,
+                    onClickFavorite = viewModel::onClickAddFavorites
+                )
             }
         }
     }
