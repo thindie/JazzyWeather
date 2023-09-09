35310f0a5fbc90cef48788b34a5da4f2c8f97621
Alex <thindie1@mail.ru>
2023-09-09 12:57:49 +0200

    add label hint to WeatherInputField in location-presenter module

--- core/presentation/location-presenter/src/main/java/com/example/thindie/location_presenter/components/WeatherInputField.kt
+++ core/presentation/location-presenter/src/main/java/com/example/thindie/location_presenter/components/WeatherInputField.kt
@@ -3,14 +3,18 @@ package com.example.thindie.location_presenter.components
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.thindie.designsystem.composables.inputfield.InputFieldState
+import com.example.thindie.designsystem.composables.inputfield.rememberInputFieldState
+import com.example.thindie.presentation.R
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun WeatherInputField(modifier: Modifier = Modifier, state: InputFieldState) {
     OutlinedTextField(
@@ -19,6 +23,15 @@ internal fun WeatherInputField(modifier: Modifier = Modifier, state: InputFieldS
             .fillMaxWidth(state.animatedWidth.value),
         value = state.fieldValue.value.value,
         onValueChange = state::onValueChange,
-        shape = RoundedCornerShape(20.dp)
+        shape = RoundedCornerShape(20.dp),
+        label = { Text(text = stringResource(id = R.string.hint_searh)) }
     )
+}
+
+@Composable
+@Preview(showBackground = true, device = Devices.PIXEL_2)
+internal fun previewWeatherInputField() {
+    com.example.thindie.designsystem.theme.JazzyWeatherTheme {
+        WeatherInputField(state = rememberInputFieldState())
+    }
 }
