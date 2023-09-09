9f46e4f1f30b514253bef1fbfb2e24daf6441d0c
Alex <thindie1@mail.ru>
2023-09-09 12:59:01 +0200

    add RememberWeatherUseCase and onClickFavorites fun to LocationPickerViewModel

--- core/presentation/location-presenter/src/main/java/com/example/thindie/location_presenter/viewmodel/LocationPickerViewModel.kt
+++ core/presentation/location-presenter/src/main/java/com/example/thindie/location_presenter/viewmodel/LocationPickerViewModel.kt
@@ -2,8 +2,10 @@ package com.example.thindie.location_presenter.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.thindie.domain.entities.ForecastAble
 import com.example.thindie.domain.entities.WeatherLocation
 import com.example.thindie.domain.usecases.GetLocationUseCase
+import com.example.thindie.domain.usecases.RememberWeatherSiteUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,9 +14,13 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 @HiltViewModel
-internal class LocationPickerViewModel @Inject constructor(private val getLocation: GetLocationUseCase) :
+internal class LocationPickerViewModel @Inject constructor(
+    private val getLocation: GetLocationUseCase,
+    private val rememberWeatherSiteUseCase: RememberWeatherSiteUseCase,
+) :
     ViewModel() {
 
     private val _uiState = MutableStateFlow(LocationsScreenState())
@@ -31,6 +37,12 @@ internal class LocationPickerViewModel @Inject constructor(private val getLocati
                 _uiState.tryEmit(LocationsScreenState(it))
             }
             .launchIn(viewModelScope)
+    }
+
+    fun onClickAddFavorites(forecastAble: ForecastAble) {
+        viewModelScope.launch {
+            rememberWeatherSiteUseCase(forecastAble)
+        }
     }
 
     data class LocationsScreenState(val locationsList: List<WeatherLocation> = emptyList())
