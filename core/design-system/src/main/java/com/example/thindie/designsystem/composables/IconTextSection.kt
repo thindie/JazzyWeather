6e6de51e803fc51414a1be70fc601c60b6e54173
Alex <thindie1@mail.ru>
2023-09-20 22:32:58 +0200

    refactor IconTextSection. moved to design-system module

---
+++ core/design-system/src/main/java/com/example/thindie/designsystem/composables/IconTextSection.kt
@@ -0,0 +1,44 @@
+package com.example.thindie.designsystem.composables
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.Text
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun IconTextSection(
+    modifier: Modifier,
+    @DrawableRes icon: Int,
+    title: String,
+    color: Color = MaterialTheme.colorScheme.onTertiary,
+) {
+    Row(
+        modifier = modifier
+            .wrapContentHeight()
+            .wrapContentWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            modifier = modifier.padding(horizontal = 8.dp),
+            painter = painterResource(id = icon),
+            contentDescription = null,
+            tint = color
+        )
+        Text(
+            text = title,
+            style = MaterialTheme.typography.labelMedium.copy(
+                color
+            )
+        )
+    }
+}
