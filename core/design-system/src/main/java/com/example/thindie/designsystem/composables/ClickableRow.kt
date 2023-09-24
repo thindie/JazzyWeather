13a59b7dfed6b510ebbaf692c17259ad608b8edd
Alex <thindie1@mail.ru>
2023-09-24 23:38:44 +0200

    design-system module: add ClickAbleRow composable

---
+++ core/design-system/src/main/java/com/example/thindie/designsystem/composables/ClickableRow.kt
@@ -0,0 +1,72 @@
+package com.example.thindie.designsystem.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.thindie.designsystem.NavigationAble
+import com.example.thindie.designsystem.navList
+
+@Composable
+fun ClickAbleRow(
+    modifier: Modifier = Modifier,
+    rowColor: Brush,
+    contentColor: Color,
+    list: List<NavigationAble>,
+    onClick: (String) -> Unit,
+) {
+    Row(
+        modifier = modifier
+            .background(brush = rowColor)
+            .height(70.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        LazyRow(
+            modifier = modifier
+                .fillMaxWidth(0.6f)
+                .fillMaxHeight(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            items(list) {
+                IconButton(onClick = { onClick(it.getRoute()) }) {
+                    Icon(
+                        painter = painterResource(id = it.getVision()),
+                        contentDescription = it.getRoute(),
+                        tint = contentColor
+                    )
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true, device = Devices.PIXEL_2)
+internal fun previewClickAbleRow() {
+    com.example.thindie.designsystem.theme.JazzyWeatherTheme {
+        ClickAbleRow(
+            rowColor = Brush.verticalGradient(listOf(Color.White, Color.Blue)),
+            contentColor = Color.White,
+            list = navList,
+            onClick = {})
+    }
+}
