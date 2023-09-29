be7ee41fb193495df820ca694046c511d24b3604
Alex <thindie1@mail.ru>
2023-09-29 12:03:21 +0200

    database module: refactor:
    add opportunity to return Flow of database, add opportunity to request weathers by
    its coordinates

--- core/database/src/main/java/com/example/thindie/database/room/WeatherHourlyDao.kt
+++ core/database/src/main/java/com/example/thindie/database/room/WeatherHourlyDao.kt
@@ -4,14 +4,18 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface WeatherHourlyDao {
     @Query("SELECT * FROM weather_hourly ORDER BY place DESC")
-    suspend fun getAllWeathersSite(): List<WeatherHourlyDbModel>
+     fun observeAllWeathersSite(): Flow<List<WeatherHourlyDbModel>>
 
     @Query("SELECT * FROM weather_hourly WHERE place LIKE :place LIMIT 1")
     suspend fun getWeatherSite(place: String): WeatherHourlyDbModel
+
+    @Query("SELECT * FROM weather_hourly WHERE latitude = :latitude AND longitude = :longitude LIMIT 1")
+    suspend fun getWeatherSite(latitude: Double, longitude: Double): WeatherHourlyDbModel
 
     @Upsert
     suspend fun upsertWeatherSite(weatherDailyDbModel: WeatherHourlyDbModel)
