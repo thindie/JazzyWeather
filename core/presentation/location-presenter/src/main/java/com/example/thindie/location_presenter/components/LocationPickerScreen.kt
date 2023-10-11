4916a61a9a99dea2934b1fda556c24679ef099c0
Alex <thindie1@mail.ru>
2023-10-11 17:25:20 +0200

    location picker: try animated visibility in required context

--- core/presentation/location-presenter/src/main/java/com/example/thindie/location_presenter/components/LocationPickerScreen.kt
+++ core/presentation/location-presenter/src/main/java/com/example/thindie/location_presenter/components/LocationPickerScreen.kt
@@ -1,36 +1,23 @@
 package com.example.thindie.location_presenter.components
 
-import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose.JazzyWeatherTheme
-import com.example.thindie.designsystem.composables.IconTextSection
-import com.example.thindie.designsystem.utils.TransGradientVerticalInverse
 import com.example.thindie.domain.entities.WeatherLocation
-import com.example.thindie.presentation.R
 
 @Suppress("LongParameterList")
 @Composable
@@ -49,7 +36,9 @@ internal fun LocationPickerScreen(
         Column(
             modifier = modifier
                 .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background.TransGradientVerticalInverse())
+                .background(
+                    MaterialTheme.colorScheme.background
+                )
         ) {
             OutlinedTextField(
                 modifier = modifier
@@ -60,63 +49,25 @@ internal fun LocationPickerScreen(
                 shape = MaterialTheme.shapes.large
             )
 
-            if (focusedLocation != null) {
-                Surface(
-                    modifier = modifier
-                        .padding(horizontal = 8.dp)
-                        .clip(MaterialTheme.shapes.medium)
-                        .fillMaxWidth()
-                        .height(80.dp)
-                        .animateContentSize(),
-                    tonalElevation = 35.dp,
-                    shadowElevation = 35.dp,
-
-                    ) {
-                    Row(
-                        modifier = modifier
-                            .padding(horizontal = 8.dp),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        if (isFocusedLocationRemembered)
-                            Icon(
-                                painter = painterResource(id = R.drawable.nav_icon_favorite),
-                                contentDescription = null
-                            )
-
-                        IconTextSection(
-                            modifier = modifier,
-                            icon = R.drawable.icon_population,
-                            title = focusedLocation.population,
-                            color = MaterialTheme.colorScheme.onSurface
-                        )
-                        IconTextSection(
-                            modifier = modifier,
-                            icon = R.drawable.icon_latitude,
-                            title = focusedLocation.latitude,
-                            color = MaterialTheme.colorScheme.onSurface
-                        )
-                        IconTextSection(
-                            modifier = modifier,
-                            icon = R.drawable.icon_longitude,
-                            title = focusedLocation.longitude,
-                            color = MaterialTheme.colorScheme.onSurface
-                        )
-
-                        OutlinedButton(onClick = { onRememberLocation(focusedLocation) }) {
-                            Text(text = stringResource(id = R.string.text_label_expecting))
+            AnimatedVisibility(visible = focusedLocation != null) {
+                if (focusedLocation != null)
+                    SelectedLocationPlanchette(
+                        modifier = modifier,
+                        isFocusedLocationRemembered = isFocusedLocationRemembered,
+                        focusedLocation = focusedLocation,
+                        onRememberLocation = onRememberLocation
+                    )
+            }
+            AnimatedVisibility(visible = fieldValue.isNotBlank()) {
+                LazyColumn {
+                    items(locations, key = { it.hashCode() }) { weatherLocation ->
+                        LocationPickerUnit(location = weatherLocation) {
+                            onFocusLocation(weatherLocation)
                         }
                     }
                 }
             }
 
-            LazyColumn {
-                items(locations, key = { it.hashCode() }) {
-                    LocationPickerUnit(location = it) {
-                        onFocusLocation(it)
-                    }
-                }
-            }
         }
     }
 }
