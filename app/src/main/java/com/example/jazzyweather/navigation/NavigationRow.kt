8b51a9e1258804bc89293487fcae62f86dcacadf
Alex <thindie1@mail.ru>
2023-10-13 21:59:53 +0200

    app: refactor possibility row design

--- app/src/main/java/com/example/jazzyweather/navigation/NavigationRow.kt
+++ app/src/main/java/com/example/jazzyweather/navigation/NavigationRow.kt
@@ -4,12 +4,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.example.thindie.designsystem.composables.ClickAbleRow
-import com.example.thindie.designsystem.utils.TransGradientVertical
+import com.example.thindie.designsystem.utils.TransGradientVerticalInverse
 
 @Composable
 fun NavigationRow(state: NavigationState, clickAbleContent: @Composable () -> Unit) {
     ClickAbleRow(
-        rowColor = MaterialTheme.colorScheme.primary.TransGradientVertical(),
+        rowColor = MaterialTheme.colorScheme.surfaceTint.TransGradientVerticalInverse(),
         contentColor = Color.White,
         list = navigationAbles,
         onClick = {
