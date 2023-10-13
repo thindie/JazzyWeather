50754e476f185eb9eb8f61bde31dcf6d33cad01a
Alex <thindie1@mail.ru>
2023-10-13 22:04:36 +0200

    favorites: refactor design favorite screen

--- core/presentation/weathers-site-favorites/src/main/java/com/example/thindie/weathers_site_favorites/screen/WeatherFavoriteScreenState.kt
+++ core/presentation/weathers-site-favorites/src/main/java/com/example/thindie/weathers_site_favorites/screen/WeatherFavoriteScreenState.kt
@@ -1,16 +1,20 @@
 package com.example.thindie.weathers_site_favorites.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.thindie.designsystem.LoadingOrShowContent
+import com.example.thindie.designsystem.utils.TransGradientVertical
 import com.example.thindie.domain.entities.ForecastAble
+import com.example.thindie.weathers_site_favorites.components.WeatherHourlySection
 import com.example.thindie.weathers_site_favorites.viewmodel.WeatherFavoritesViewModel
 
 @Composable
@@ -31,15 +35,29 @@ fun WeatherFavoriteScreenState(
         tint = MaterialTheme.colorScheme.surface,
         isLoading = favoritesScreenState.value.isLoading
     ) {
-        LazyColumn() {
-            items(favoritesScreenState.value.list) { weatherHourly ->
-                ClickableText(
-                    text = AnnotatedString(weatherHourly.place),
-                    onClick = { onClickNavigation(weatherHourly) })
-
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    MaterialTheme
+                        .colorScheme
+                        .surfaceTint
+                        .TransGradientVertical()
+                )
+        ) {
+            LazyColumn(
+            ) {
+                items(favoritesScreenState.value.list) { weatherHourly ->
+                    WeatherHourlySection(
+                        hourly = weatherHourly,
+                        onDecodeWeatherCode = viewModel::onDecodeWeatherCode,
+                        onClickConcrete = onClickNavigation,
+                        currentHour = favoritesScreenState.value.currentHour
+                    )
+                }
             }
         }
-    }
 
+    }
 }
 
