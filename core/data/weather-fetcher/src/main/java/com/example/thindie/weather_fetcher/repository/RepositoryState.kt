6e70d8bee4df3f4195e2795610363e8d5281cb4c
Alex <thindie1@mail.ru>
2023-10-19 22:44:02 +0200

    data: refactor repository state

--- core/data/weather-fetcher/src/main/java/com/example/thindie/weather_fetcher/repository/RepositoryState.kt
+++ core/data/weather-fetcher/src/main/java/com/example/thindie/weather_fetcher/repository/RepositoryState.kt
@@ -3,8 +3,6 @@ package com.example.thindie.weather_fetcher.repository
 import com.example.thindie.domain.entities.ForecastAble
 
 data class RepositoryState(
-    val isFetchAllowed: Boolean,
-    val isNetworkAvailAble: Boolean,
     val forecastAble: ForecastAble?,
     val foreCastAbleList: List<ForecastAble>,
 )
