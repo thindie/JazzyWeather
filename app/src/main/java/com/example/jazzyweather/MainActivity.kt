108acf2b23d315bad4b37ce6d6efe98bf157b6bb
Alex <thindie1@mail.ru>
2023-10-19 22:42:19 +0200

    app module: add logic to early app hotting

--- app/src/main/java/com/example/jazzyweather/MainActivity.kt
+++ app/src/main/java/com/example/jazzyweather/MainActivity.kt
@@ -5,41 +5,74 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import com.example.thindie.designsystem.theme.JazzyWeatherTheme
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.lifecycleScope
 import com.example.jazzyweather.navigation.WeatherScreen
 import com.example.jazzyweather.navigation.rememberNavigationState
 import com.example.jazzyweather.themeChanger.ThemeChanger
 import com.example.thindie.designsystem.TransparentSystemBars
+import com.example.thindie.designsystem.theme.JazzyWeatherTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private val mainViewModel: MainViewModel by viewModels()
+    private val currentScreen = mutableStateOf(WeatherScreen.LOCATION_PICKER)
+
+    override fun onStart() {
+        super.onStart()
+        lifecycleScope.launch {
+            mainViewModel.destinationState
+                .onEach { currentScreen.value = it }
+                .launchIn(this)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainViewModel.onStart()
         setContent {
+            var isFirstLaunch by rememberSaveable {
+                mutableStateOf(true)
+            }
             val darken = isSystemInDarkTheme()
             val navigationState = rememberNavigationState()
             val isSystemInDark = rememberSaveable() {
                 mutableStateOf(darken)
             }
+
+            LaunchedEffect(true) {
+                if (isFirstLaunch) {
+                    delay(mainViewModel.hottingTime)
+                    isFirstLaunch = false
+                }
+            }
+
             JazzyWeatherTheme(useDarkTheme = isSystemInDark.value) {
-                TransparentSystemBars(isSystemInDark.value)
-                WeatherApp(
-                    viewModel = mainViewModel,
-                    weatherScreen = WeatherScreen.LOCATION_PICKER,
-                    state = navigationState,
-                    themeChanger = {
-                        ThemeChanger {
-                            isSystemInDark.value = !isSystemInDark.value
+                if (!isFirstLaunch) {
+                    TransparentSystemBars(isSystemInDark.value)
+                    WeatherApp(
+                        viewModel = mainViewModel,
+                        weatherScreen = currentScreen.value,
+                        state = navigationState,
+                        themeChanger = {
+                            ThemeChanger {
+                                isSystemInDark.value = !isSystemInDark.value
+                            }
                         }
-                    }
-                )
+                    )
+                } else {
+                    CircularProgressIndicator()
+                }
             }
         }
     }
--- app/src/main/java/com/example/jazzyweather/MainViewModel.kt
+++ app/src/main/java/com/example/jazzyweather/MainViewModel.kt
@@ -3,30 +3,42 @@ package com.example.jazzyweather
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.jazzyweather.navigation.WeatherScreen
+import com.example.thindie.domain.RatificationObserver
 import com.example.thindie.domain.entities.ForecastAble
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import javax.inject.Named
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEmpty
+import kotlinx.coroutines.flow.stateIn
 
 @HiltViewModel
-class MainViewModel @Inject constructor() :
+class MainViewModel @Inject constructor(@Named("volumeController") private val controller: RatificationObserver) :
     ViewModel() {
     private val _hottingTime = 4000L
-    val hottingTime = _hottingTime.toInt()
+    val hottingTime = _hottingTime
 
-    private val _destinationState = MutableStateFlow(WeatherScreen.ALL)
-    val destinationState = _destinationState.asStateFlow()
+    private val _destinationState = controller.observeRatification()
+        .onEmpty { WeatherScreen.LOCATION_PICKER }
+        .map { permission ->
+            if (permission.isAllowed())
+                WeatherScreen.ALL
+            else WeatherScreen.LOCATION_PICKER
+        }
+
+    val destinationState = _destinationState.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000L),
+        WeatherScreen.LOCATION_PICKER
+    )
 
     private val _forecastAbleState: MutableStateFlow<ForecastAble?> = MutableStateFlow(null)
     val forecastAbleState: StateFlow<ForecastAble?> = _forecastAbleState.asStateFlow()
-    fun onStart() {
-        viewModelScope.launch {
 
-        }
-    }
 
     fun onChoseForecastAble(forecastAble: ForecastAble) {
         _forecastAbleState.value = forecastAble
