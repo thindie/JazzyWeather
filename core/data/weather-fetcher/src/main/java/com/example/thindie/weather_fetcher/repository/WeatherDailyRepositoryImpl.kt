47b85b35c4becdf5c9c01f5467672a0af5a57a7b
Alex <thindie1@mail.ru>
2023-10-20 17:53:32 +0200

    data: deep refactor repo - group logic

--- core/data/weather-fetcher/src/main/java/com/example/thindie/weather_fetcher/repository/WeatherDailyRepositoryImpl.kt
+++ core/data/weather-fetcher/src/main/java/com/example/thindie/weather_fetcher/repository/WeatherDailyRepositoryImpl.kt
@@ -5,62 +5,91 @@ import com.example.thindie.core.network.di.DispatchersIOModule
 import com.example.thindie.database.room.WeatherDailyDao
 import com.example.thindie.database.room.WeatherHourlyDao
 import com.example.thindie.domain.ForecastAbleRepository
-import com.example.thindie.domain.RatificationObserver
 import com.example.thindie.domain.entities.ForecastAble
 import com.example.thindie.domain.entities.WeatherDaily
+import com.example.thindie.weather_fetcher.mappers.map
 import javax.inject.Inject
-import javax.inject.Named
 import javax.inject.Singleton
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Singleton
 internal class WeatherDailyRepositoryImpl @Inject constructor(
     private val dailyDao: WeatherDailyDao,
     private val hourlyDao: WeatherHourlyDao,
     private val apiService: WeatherApiService,
-    @Named("fetchController") private val fetchController: RatificationObserver,
-    @Named("networkController") private val netWorkController: RatificationObserver,
     @DispatchersIOModule.IODispatcher private val ioDispatcher: CoroutineDispatcher,
 ) : ForecastAbleRepository<WeatherDaily> {
 
-    private val state = MutableStateFlow(
-        RepositoryState(
-            forecastAble = null,
-            isFetchAllowed = false,
-            isNetworkAvailAble = false,
-            foreCastAbleList = emptyList()
-        )
-    )
+    private val place = MutableStateFlow<String?>(null)
 
+    private val forecastAble = callbackFlow {
+        launch {
+            val fromBase = IO {
+                dailyDao.getWeatherSite(place = place.value.orEmpty())
+            }
+            send(fromBase.map())
+        }
+        awaitClose {
+           // place.tryEmit(null)
+        }
+    }.flowOn(ioDispatcher)
 
     override suspend fun requestForecastAble(forecastAble: ForecastAble) {
-        TODO("Not yet implemented")
+        place.value = forecastAble.getSight()
     }
 
     override suspend fun requestConcreteDateForecastAble(
         simpleIso8106: String,
         forecastAble: ForecastAble,
     ) {
-        TODO("Not yet implemented")
+        IO {
+
+        }
     }
 
     override fun observeForecastAbleList(): Flow<List<WeatherDaily>> {
-        TODO("Not yet implemented")
+        return flow { }
     }
 
     override fun observeForecastAble(): Flow<WeatherDaily> {
-        TODO("Not yet implemented")
+        return forecastAble
+            .flowOn(ioDispatcher)
     }
 
     override suspend fun deleteForecastAble(place: String) {
-        TODO("Not yet implemented")
+        IO {
+            dailyDao.deleteWeatherSite(place)
+        }
+
     }
 
     override suspend fun rememberWeatherLocation(forecastAble: ForecastAble) {
-        TODO("Not yet implemented")
+        val daily = IO {
+            dailyDao.getWeatherSite(
+                forecastAble.getSightLatitude().toDouble(),
+                forecastAble.getSightLongitude().toDouble()
+            )
+        }
+
+        deleteForecastAble(daily.place)
+        IO {
+            dailyDao.upsertWeatherSite(daily.copy(place = forecastAble.getSight()))
+        }
+
     }
 
+    private suspend fun <T> IO(action: suspend () -> T): T {
+        return withContext(ioDispatcher) {
+            action.invoke()
+        }
+    }
 
 }
--- core/data/weather-fetcher/src/main/java/com/example/thindie/weather_fetcher/repository/WeatherHourlyRepositoryImpl.kt
+++ core/data/weather-fetcher/src/main/java/com/example/thindie/weather_fetcher/repository/WeatherHourlyRepositoryImpl.kt
@@ -2,125 +2,77 @@ package com.example.thindie.weather_fetcher.repository
 
 import com.example.thindie.core.network.WeatherApiService
 import com.example.thindie.core.network.di.DispatchersIOModule
-import com.example.thindie.core.network.dto.hourlydto.map
-import com.example.thindie.database.room.WeatherDailyDao
 import com.example.thindie.database.room.WeatherHourlyDao
-import com.example.thindie.database.room.WeatherHourlyDbModel
 import com.example.thindie.domain.ForecastAbleRepository
-import com.example.thindie.domain.RatificationObserver
 import com.example.thindie.domain.entities.ForecastAble
 import com.example.thindie.domain.entities.WeatherHourly
-import com.example.thindie.weather_fetcher.FetchUnit
 import com.example.thindie.weather_fetcher.mappers.map
-import com.example.thindie.weather_fetcher.toFetchUnit
 import javax.inject.Inject
-import javax.inject.Named
 import javax.inject.Singleton
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.filterNot
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 
 @Singleton
 internal class WeatherHourlyRepositoryImpl @Inject constructor(
     private val hourlyDao: WeatherHourlyDao,
     private val apiService: WeatherApiService,
-    @Named("fetchController") private val fetchController: RatificationObserver,
-    @Named("networkController") private val netWorkController: RatificationObserver,
     @DispatchersIOModule.IODispatcher private val ioDispatcher: CoroutineDispatcher,
 ) : ForecastAbleRepository<WeatherHourly> {
 
-    private val fetchQueue: MutableStateFlow<FetchUnit?> = MutableStateFlow(null)
-
-    private val state: Flow<RepositoryState> =
-        combine(
-            fetchQueue,
-            fetchController.observeRatification(),
-            netWorkController.observeRatification(),
-            hourlyDao.observeAllWeathersSite(),
-        ) { fetchUnit, fetchController, networkController, storedUnits ->
-
-            if (networkController.isAllowed()) {
-                if (fetchUnit?.concreteTime != null) {
-                    apiService.getHourlyWeatherByDate(
-                        iso8106String = fetchUnit.concreteTime,
-                        latitude = fetchUnit.latitude,
-                        longitude = fetchUnit.longitude,
-                        timeZone = fetchUnit.timezone
-                    )
-                }
-                if (fetchController.isAllowed()) {
-                    updateStored(storedUnits)
-                }
-            }
-
-            RepositoryState(
-                forecastAble = null,
-                isFetchAllowed = fetchController.isAllowed(),
-                isNetworkAvailAble = networkController.isAllowed(),
-                foreCastAbleList = storedUnits.map { it.map() }
-            )
-        }
-
 
     override suspend fun requestForecastAble(forecastAble: ForecastAble) {
-        fetchQueue.value = forecastAble.toFetchUnit(null)
+        IO {
+
+        }
     }
 
     override suspend fun requestConcreteDateForecastAble(
         simpleIso8106: String,
         forecastAble: ForecastAble,
     ) {
-        fetchQueue.value = forecastAble.toFetchUnit(simpleIso8106)
+        IO { }
     }
 
     override fun observeForecastAbleList(): Flow<List<WeatherHourly>> {
-        return state.map { state ->
-            state
-                .foreCastAbleList
-                .filter { forecastAble ->
-                    forecastAble::class.java.isAssignableFrom(WeatherHourly::class.java)
-                }
-                .map { forecastAble ->
-                    forecastAble as WeatherHourly
-                }
+        return hourlyDao.observeAllWeathersSite().map {
+            it.map {
+                it.map()
+            }
         }.flowOn(ioDispatcher)
     }
 
     override fun observeForecastAble(): Flow<WeatherHourly> {
-        return state
-            .filterNot { state ->
-                state.forecastAble == null
-            }.map { state ->
-                requireNotNull(state.forecastAble)
-                require(state.forecastAble::class.java.isAssignableFrom(WeatherHourly::class.java))
-                state.forecastAble as WeatherHourly
-            }.flowOn(ioDispatcher)
+        return flow {}
     }
-
 
     override suspend fun deleteForecastAble(place: String) {
-        hourlyDao.deleteWeatherSite(place)
-    }
-
-    override suspend fun rememberWeatherLocation(forecastAble: ForecastAble) {
-        if (forecastAble::class.java.isAssignableFrom(WeatherHourly::class.java)) {
-            forecastAble as WeatherHourly
-            hourlyDao.upsertWeatherSite(forecastAble.map())
+        IO {
+                hourlyDao.deleteWeatherSite(place)
         }
     }
 
-    private suspend fun updateStored(list: List<WeatherHourlyDbModel>) {
-        list.forEach {
-            val fetched = apiService.getHourlyWeather(
-                latitude = it.latitude.toFloat(),
-                longitude = it.longitude.toFloat(),
-                timeZone = it.timezone
+    override suspend fun rememberWeatherLocation(forecastAble: ForecastAble) {
+        val hourly = IO {
+            hourlyDao.getWeatherSite(
+                forecastAble.getSightLatitude().toDouble(),
+                forecastAble.getSightLongitude().toDouble()
             )
-            hourlyDao.upsertWeatherSite(fetched.map().map(it.map()).map())
+        }
+
+        deleteForecastAble(hourly.place)
+        IO {
+            hourlyDao.upsertWeatherSite(hourly.copy(place = forecastAble.getSight()))
+        }
+    }
+
+
+    private suspend fun <T> IO(action: suspend () -> T): T {
+        return withContext(ioDispatcher) {
+            action.invoke()
         }
     }
 
