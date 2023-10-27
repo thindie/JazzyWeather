748c6ef4a36f44b08ff424e76159d6b1ed0cb594
Alex <thindie1@mail.ru>
2023-10-27 18:24:06 +0200

    pickerscreen: refactor: decomposed main picker screen, add right anim
    for debounce ai, fix alignment and arrangements

--- core/presentation/location-presenter/src/main/java/com/example/thindie/location_presenter/components/LocationPickerUnit.kt
+++ core/presentation/location-presenter/src/main/java/com/example/thindie/location_presenter/components/LocationPickerUnit.kt
@@ -1,16 +1,23 @@
 package com.example.thindie.location_presenter.components
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -29,13 +36,17 @@ internal fun LocationPickerUnit(
     location: WeatherLocation,
     onClickCard: () -> Unit,
     onShowLocation: (ForecastAble) -> Unit,
+    isSelected: Boolean
 ) {
+    var isSelectedItem by rememberSaveable {
+        mutableStateOf(false)
+    }
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .clip(MaterialTheme.shapes.large)
             .height(80.dp)
             .padding(horizontal = 8.dp, vertical = 4.dp)
+            .clip(MaterialTheme.shapes.medium)
             .background(MaterialTheme.colorScheme.background)
             .clickable { onClickCard() },
         verticalAlignment = Alignment.CenterVertically,
@@ -51,7 +62,13 @@ internal fun LocationPickerUnit(
                 .titleLarge.copy(color = MaterialTheme.colorScheme.onSurface)
         ) {
             onShowLocation(location)
+            isSelectedItem = !isSelectedItem
         }
+        Spacer(modifier = Modifier.fillMaxWidth(0.8f))
+        AnimatedVisibility(visible = isSelected) {
+            Text(text = ".", style = MaterialTheme.typography.headlineSmall)
+        }
+
     }
 }
 
