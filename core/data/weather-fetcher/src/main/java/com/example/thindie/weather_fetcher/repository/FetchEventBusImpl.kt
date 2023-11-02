27f6267a78242352a971d8f6697a743e5ae00aef
Alex <thindie1@mail.ru>
2023-11-02 13:46:44 +0200

    data: add dependency for FetchEventBusImpl by IOdispatchers

--- core/data/weather-fetcher/src/main/java/com/example/thindie/weather_fetcher/repository/FetchEventBusImpl.kt
+++ core/data/weather-fetcher/src/main/java/com/example/thindie/weather_fetcher/repository/FetchEventBusImpl.kt
@@ -1,5 +1,6 @@
 package com.example.thindie.weather_fetcher.repository
 
+import com.example.thindie.core.localrawresources.di.DispatchersIOModule
 import com.example.thindie.domain.EventBus
 import com.example.thindie.domain.EventClass
 import com.example.thindie.domain.EventTransmitter
@@ -7,17 +8,21 @@ import com.example.thindie.weather_fetcher.EventKind
 import com.example.thindie.weather_fetcher.mappers.toRes
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flowOn
 
 @Singleton
-internal class FetchEventBusImpl @Inject constructor() : EventBus<Int>,
+internal class FetchEventBusImpl
+@Inject constructor(@DispatchersIOModule.IODispatcher private val dispatchersIO: CoroutineDispatcher) :
+    EventBus<Int>,
     EventTransmitter<EventKind> {
 
     private val _eventFlow = MutableStateFlow<EventClass<Int>?>(null)
     override fun observeEvents(): Flow<EventClass<Int>> {
-        return _eventFlow.filterNotNull()
+        return _eventFlow.filterNotNull().flowOn(dispatchersIO)
     }
 
     override fun send(eventClass: EventClass<EventKind>) {
