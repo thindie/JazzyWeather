27a406aecc8274b36645f0530d4b16905fbc582a
Alex <thindie1@mail.ru>
2024-01-18 09:53:06 +0200

    app database: fix typeConverter issue

--- core/design-system/src/main/java/com/example/thindie/designsystem/TransparentStatusBar.kt
+++ core/design-system/src/main/java/com/example/thindie/designsystem/TransparentStatusBar.kt
@@ -1,6 +1,5 @@
 package com.example.thindie.designsystem
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
